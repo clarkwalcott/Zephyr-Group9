3af566f5e84e65609720c0e272186bf0f8a46717
Clark Walcott <clark.walcott@gmail.com>
2019-04-12 15:33:56 -0500

    Added files. Still need to be stubbed out

---
+++ info.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Info</title>
+	<link rel="stylesheet" href="app.css">
+	<script src="app.js"></script> 
+</head>
+<body>
+	<?php
+
+
+	?>
+</body>
+</html>
